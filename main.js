2955f45438b2da23c0dc98b973cba1d387474ed9
¨Matias <matias.yanke@outlook.com>
2024-01-10 19:24:33 -0300

    2da preentrega

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
-
+// Función para calcular el IMC
 function calcularIMC(peso, altura) {
     return peso / (altura * altura);
 }
 
-//Ingreso de datos
+// Función para obtener datos del usuario
+function obtenerDatosUsuario() {
+    let datos = {};
 
-let peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
-let altura = parseFloat(prompt("Ingrese su altura en metros:"));
+    datos.peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
+    datos.altura = parseFloat(prompt("Ingrese su altura en metros:"));
 
-// Verificacion
+    while (isNaN(datos.peso) || isNaN(datos.altura) || datos.peso <= 0 || datos.altura <= 0) {
+        alert("Por favor, ingrese datos válidos.");
+        datos.peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
+        datos.altura = parseFloat(prompt("Ingrese su altura en metros:"));
+    }
 
-while (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
-    alert("Por favor, ingrese datos válidos.");
-    peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
-    altura = parseFloat(prompt("Ingrese su altura en metros:"));
+    return datos;
 }
 
-//Calculo
-
-const imc = calcularIMC(peso, altura);
-
-//Resultados
-
-let mensaje = "Su índice de masa corporal (IMC) es: " + imc.toFixed(2) + "\n\n";
-if (imc < 18.5) {
-    mensaje += "Usted está bajo peso.";
-} else if (imc >= 18.5 && imc < 24.9) {
-    mensaje += "Usted tiene un peso saludable.";
-} else if (imc >= 25 && imc < 29.9) {
-    mensaje += "Usted está en sobrepeso.";
-} else {
-    mensaje += "Usted tiene obesidad.";
+// Función para calcular el IMC y devolver un mensaje
+function interpretarIMC(imc) {
+    if (imc < 18.5) {
+        return "Usted está bajo peso.";
+    } else if (imc >= 18.5 && imc < 24.9) {
+        return "Usted tiene un peso saludable.";
+    } else if (imc >= 25 && imc < 29.9) {
+        return "Usted está en sobrepeso.";
+    } else {
+        return "Usted tiene obesidad.";
+    }
 }
-alert(mensaje);
+
+//Llamar a las funciones anteriores
+function ejecutarPrograma() {
+    
+    const datosUsuario = obtenerDatosUsuario();
+
+    
+    const imc = calcularIMC(datosUsuario.peso, datosUsuario.altura);
+
+   
+    const mensaje = "Su índice de masa corporal (IMC) es: " + imc.toFixed(2) + "\n\n" + interpretarIMC(imc);
+
+    
+    alert(mensaje);
+}
+
+
+ejecutarPrograma();
