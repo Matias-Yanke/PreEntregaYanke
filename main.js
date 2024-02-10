8d5f1e9d13671a92d870b10feb65ae172968d707
¨Matias <matias.yanke@outlook.com>
2024-02-10 18:05:50 -0300

    proyectoFinalYanke

--- main.js
+++ main.js
@@ -1,121 +1,131 @@
-// Función para calcular el IMC
-function calcularIMC(peso, altura) {
-    return peso / (altura * altura);
-}
+document.addEventListener('DOMContentLoaded', async () => {
+    // Array para almacenar los usuarios
+    let usuarios = [];
 
-// Datos del usuario
-function obtenerDatosUsuario() {
-    const nombreInput = document.getElementById('nombre');
-    const pesoInput = document.getElementById('peso');
-    const alturaInput = document.getElementById('altura');
-
-    const datos = {
-        nombre: nombreInput.value,
-        peso: parseFloat(pesoInput.value),
-        altura: parseFloat(alturaInput.value),
-    };
-
-    if (
-        isNaN(datos.peso) ||
-        isNaN(datos.altura) ||
-        datos.peso <= 0 ||
-        datos.altura <= 0 ||
-        datos.nombre.trim() === ""
-    ) {
-        alert("Por favor, ingrese datos válidos.");
-        return null;
+    // Cargar usuarios desde el archivo JSON al iniciar la página
+    const usuariosJSON = await fetch('./usuarios.json');
+    if (usuariosJSON.ok) {
+        usuarios = await usuariosJSON.json();
     }
 
-    datos.imc = calcularIMC(datos.peso, datos.altura);
+    // Función para calcular el Índice de Masa Corporal (IMC) de un usuario
+    function calcularIMC(peso, altura) {
+        return peso / (altura * altura);
+    }
 
-    return datos;
-}
+    // Función para obtener los datos del usuario ingresados en el formulario
+    function obtenerDatosUsuario() {
+        const nombreInput = document.getElementById('nombre');
+        const pesoInput = document.getElementById('peso');
+        const alturaInput = document.getElementById('altura');
 
-// IMC más alto en el array
-function buscarIMCMaximo(usuarios) {
-    return usuarios.reduce((max, usuario) => (usuario.imc > max.imc ? usuario : max), usuarios[0]);
-}
+        const nombre = nombreInput.value.trim();
+        const peso = parseFloat(pesoInput.value);
+        const altura = parseFloat(alturaInput.value);
 
-// IMC más bajo en el array
-function buscarIMCMinimo(usuarios) {
-    return usuarios.reduce((min, usuario) => (usuario.imc < min.imc ? usuario : min), usuarios[0]);
-}
+        // Validar que los datos ingresados sean correctos
+        if (!nombre || isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+            alert("Por favor, ingrese datos válidos.");
+            return null;
+        }
 
-// Mostrar resultado de usuario en el DOM
-function mostrarResultadoUsuario(usuario) {
-    const resultadoContainer = document.getElementById('resultado-container');
-    resultadoContainer.innerHTML = `
-        <p>Usuario destacado:</p>
-        <p>Nombre: ${usuario.nombre}</p>
-        <p>IMC: ${usuario.imc.toFixed(2)}</p>
-    `;
-}
+        // Calcular el IMC del usuario
+        const imc = calcularIMC(peso, altura);
 
-// Agregar usuarios al array y almacenar en Local Storage
-function agregarUsuarioYActualizarDOM() {
-    const datosUsuario = obtenerDatosUsuario();
+        return { nombre, peso, altura, imc };
+    }
 
-    if (datosUsuario) {
-        usuarios.push(datosUsuario);
+    // Función para buscar el usuario con el IMC más alto en el array de usuarios
+    function buscarIMCMaximo(usuarios) {
+        return usuarios.reduce((max, usuario) => (usuario.imc > max.imc ? usuario : max), usuarios[0]);
+    }
 
-        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    // Función para buscar el usuario con el IMC más bajo en el array de usuarios
+    function buscarIMCMinimo(usuarios) {
+        return usuarios.reduce((min, usuario) => (usuario.imc < min.imc ? usuario : min), usuarios[0]);
+    }
 
+    // Función para mostrar los datos del usuario con IMC destacado en el DOM
+    function mostrarResultadoUsuario(usuario) {
+        const resultadoContainer = document.getElementById('resultado-container');
+        resultadoContainer.innerHTML = `
+            <p>Usuario destacado:</p>
+            <p>Nombre: ${usuario.nombre}</p>
+            <p>IMC: ${usuario.imc.toFixed(2)}</p>
+        `;
+    }
+
+    // Función para agregar un nuevo usuario y actualizar el DOM
+    function agregarUsuarioYActualizarDOM() {
+        const datosUsuario = obtenerDatosUsuario();
+
+        if (datosUsuario) {
+            usuarios.push(datosUsuario);
+
+            localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+            // alerta con libreria sweetalert
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Datos cargados con éxito",
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            actualizarDOM();
+            
+        }
+    }
+
+    // Función para eliminar todos los datos de usuarios y actualizar el DOM
+    function eliminarTodosLosDatos() {
+        usuarios.length = 0;
+        localStorage.removeItem('usuarios');
         actualizarDOM();
     }
-}
 
-// Eliminar todos los datos
-function eliminarTodosLosDatos() {
-    
-    usuarios.length = 0;
-    localStorage.removeItem('usuarios');
+    // Función para actualizar la visualización de usuarios en el DOM
+    function actualizarDOM() {
+        const contenedorUsuarios = document.getElementById('usuarios-container');
+        contenedorUsuarios.innerHTML = '';
 
-    actualizarDOM();
-}
+        usuarios.forEach(usuario => {
+            const usuarioDiv = document.createElement('div');
+            usuarioDiv.classList.add('usuario');
 
-// Lista de usuarios
-function actualizarDOM() {
-    const contenedorUsuarios = document.getElementById('usuarios-container');
-    contenedorUsuarios.innerHTML = '';
+            const contenidoUsuario = `
+                <p><strong>Nombre:</strong> ${usuario.nombre}</p>
+                <p><strong>Peso:</strong> ${usuario.peso} kg</p>
+                <p><strong>Altura:</strong> ${usuario.altura} m</p>
+                <p><strong>IMC:</strong> ${usuario.imc.toFixed(2)}</p>
+            `;
 
-    usuarios.forEach(usuario => {
-        const usuarioDiv = document.createElement('div');
-        usuarioDiv.classList.add('usuario');
+            usuarioDiv.innerHTML = contenidoUsuario;
+            contenedorUsuarios.appendChild(usuarioDiv);
+        });
 
-        const contenidoUsuario = `
-            <p><strong>Nombre:</strong> ${usuario.nombre}</p>
-            <p><strong>Peso:</strong> ${usuario.peso} kg</p>
-            <p><strong>Altura:</strong> ${usuario.altura} m</p>
-            <p><strong>IMC:</strong> ${usuario.imc.toFixed(2)}</p>
+        // Mostrar el IMC máximo y mínimo
+        const usuarioConIMCMaximo = buscarIMCMaximo(usuarios);
+        const usuarioConIMCMinimo = buscarIMCMinimo(usuarios);
+        const resultadoContainer = document.getElementById('resultado-container');
+        resultadoContainer.innerHTML = `
+            <p>Usuario con IMC más alto:</p>
+            <p>Nombre: ${usuarioConIMCMaximo.nombre}</p>
+            <p>IMC: ${usuarioConIMCMaximo.imc.toFixed(2)}</p>
+            
+            <p>Usuario con IMC más bajo:</p>
+            <p>Nombre: ${usuarioConIMCMinimo.nombre}</p>
+            <p>IMC: ${usuarioConIMCMinimo.imc.toFixed(2)}</p>
         `;
+    }
 
-        usuarioDiv.innerHTML = contenidoUsuario;
-        contenedorUsuarios.appendChild(usuarioDiv);
-    });
+    // Botones
+    document.getElementById('calcularIMCButton').addEventListener('click', agregarUsuarioYActualizarDOM);
+    document.getElementById('mostrarIMCMaximoButton').addEventListener('click', () => mostrarResultadoUsuario(buscarIMCMaximo(usuarios)));
+    document.getElementById('mostrarIMCMinimoButton').addEventListener('click', () => mostrarResultadoUsuario(buscarIMCMinimo(usuarios)));
+    document.getElementById('eliminarTodosLosDatosButton').addEventListener('click', eliminarTodosLosDatos);
 
-    // Mostrar el IMC máximo y mínimo
-    const usuarioConIMCMaximo = buscarIMCMaximo(usuarios);
-    const usuarioConIMCMinimo = buscarIMCMinimo(usuarios);
-    const resultadoContainer = document.getElementById('resultado-container');
-    resultadoContainer.innerHTML = `
-        <p>Usuario con IMC más alto:</p>
-        <p>Nombre: ${usuarioConIMCMaximo.nombre}</p>
-        <p>IMC: ${usuarioConIMCMaximo.imc.toFixed(2)}</p>
-        
-        <p>Usuario con IMC más bajo:</p>
-        <p>Nombre: ${usuarioConIMCMinimo.nombre}</p>
-        <p>IMC: ${usuarioConIMCMinimo.imc.toFixed(2)}</p>
-    `;
-}
-
-// Botones
-document.getElementById('calcularIMCButton').addEventListener('click', agregarUsuarioYActualizarDOM);
-document.getElementById('mostrarIMCMaximoButton').addEventListener('click', () => mostrarResultadoUsuario(buscarIMCMaximo(usuarios)));
-document.getElementById('mostrarIMCMinimoButton').addEventListener('click', () => mostrarResultadoUsuario(buscarIMCMinimo(usuarios)));
-document.getElementById('eliminarTodosLosDatosButton').addEventListener('click', eliminarTodosLosDatos);
-
-// Recuperar usuarios almacenados en Local Storage
-const usuariosJSON = localStorage.getItem('usuarios');
-const usuarios = usuariosJSON ? JSON.parse(usuariosJSON) : [];
-
-actualizarDOM();
+    // Actualizar el DOM al cargar la página
+    actualizarDOM();
+});
